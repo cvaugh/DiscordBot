b6c0c3f29bb3697c9f2700283e4e2b9cc4470408
Chase Vaughan <git@cvaugh.dev>
2022-10-09 23:51:14 -0400

    Add Logger class

---
+++ src/main/java/dev/cvaugh/discordbot/Logger.java
@@ -0,0 +1,27 @@
+package dev.cvaugh.discordbot;
+
+public final class Logger {
+    public static void info(String s) {
+        System.out.printf("[info] %s%n", s);
+    }
+
+    public static void info(String s, String... args) {
+        info(String.format(s, args));
+    }
+
+    public static void warn(String s) {
+        System.out.printf("[warn] %s%n", s);
+    }
+
+    public static void warn(String s, String... args) {
+        warn(String.format(s, args));
+    }
+
+    public static void error(String s) {
+        System.err.printf("[error] %s%n", s);
+    }
+
+    public static void error(String s, String... args) {
+        warn(String.format(s, args));
+    }
+}
