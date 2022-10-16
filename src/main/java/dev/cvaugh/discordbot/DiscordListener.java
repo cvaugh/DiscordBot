f1c932890edf117facdfee00d6898f15c206ffc5
Chase Vaughan <git@cvaugh.dev>
2022-10-16 04:08:09 -0400

    Add user muting

--- src/main/java/dev/cvaugh/discordbot/DiscordListener.java
+++ src/main/java/dev/cvaugh/discordbot/DiscordListener.java
@@ -4,20 +4,25 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.utils.TimeFormat;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -34,7 +39,7 @@ public class DiscordListener extends ListenerAdapter {
                 Guilds.put(id);
             }
         }
-        Main.startUpdateTask();
+        Main.scheduleUpdateTask();
     }
 
     @Override
@@ -82,6 +87,53 @@ public class DiscordListener extends ListenerAdapter {
         case "poll" -> {
             handlePollCommand(event);
         }
+        case "mute" -> {
+            Guild guild = event.getGuild();
+            if(guild == null) {
+                event.reply("This command may only be used in a server.").setEphemeral(true)
+                        .queue();
+                return;
+            }
+            if(!checkCanMute(event, guild, true))
+                return;
+            OptionMapping toMute = event.getOption("user");
+            if(toMute == null) {
+                event.reply("Missing argument: `user`").setEphemeral(true).queue();
+                return;
+            }
+            OptionMapping time = event.getOption("time");
+            if(time == null) {
+                event.reply("Missing argument: `time`").setEphemeral(true).queue();
+                return;
+            }
+            long end = Utils.durationToTimestamp(time.getAsString());
+            if(end == 0L) {
+                event.reply("Invalid duration: `" + time.getAsString() + "`").setEphemeral(true)
+                        .queue();
+                return;
+            }
+            Guilds.get(guild.getIdLong()).mute(toMute.getAsUser().getIdLong(), end);
+            event.reply(toMute.getAsUser().getAsMention() + " has been muted until " +
+                            TimeFormat.DATE_LONG.format(end) + " at " + TimeFormat.TIME_LONG.format(end))
+                    .queue();
+        }
+        case "unmute" -> {
+            Guild guild = event.getGuild();
+            if(guild == null) {
+                event.reply("This command may only be used in a server.").setEphemeral(true)
+                        .queue();
+                return;
+            }
+            if(!checkCanMute(event, guild, true))
+                return;
+            OptionMapping toUnmute = event.getOption("user");
+            if(toUnmute == null) {
+                event.reply("Missing argument: `user`").setEphemeral(true).queue();
+                return;
+            }
+            Guilds.get(guild.getIdLong()).unmute(toUnmute.getAsUser().getIdLong());
+            event.reply(toUnmute.getAsUser().getAsMention() + " has been unmuted").queue();
+        }
         case "role-assigner" -> {
             handleRoleAssignerCommand(event);
         }
@@ -96,15 +148,43 @@ public class DiscordListener extends ListenerAdapter {
             }
             GuildSettings settings = Guilds.get(guild.getIdLong());
             boolean noArgs = true;
-            OptionMapping pollRole = event.getOption("poll-role");
-            if(pollRole != null) {
+            OptionMapping roleToCreatePoll = event.getOption("role-to-poll");
+            if(roleToCreatePoll != null) {
                 noArgs = false;
-                Role role = pollRole.getAsRole();
+                Role role = roleToCreatePoll.getAsRole();
                 settings.roleToCreatePoll =
                         role.getAsMention().equals("@everyone") ? 0 : role.getIdLong();
-                ;
                 event.reply("The role required to create polls is now " + role.getAsMention() + ".")
                         .setEphemeral(true).queue();
+            }
+            OptionMapping roleToMute = event.getOption("role-to-mute");
+            if(roleToMute != null) {
+                noArgs = false;
+                Role role = roleToMute.getAsRole();
+                if(role.getAsMention().equals("@everyone")) {
+                    settings.roleToMute = 0;
+                    event.reply("Muting users is now restricted to the server owner.")
+                            .setEphemeral(true).queue();
+                } else {
+                    settings.roleToMute = role.getIdLong();
+                    event.reply(
+                                    "The role required to mute users is now " + role.getAsMention() + ".")
+                            .setEphemeral(true).queue();
+                }
+            }
+            OptionMapping mutedRole = event.getOption("muted-role");
+            if(mutedRole != null) {
+                noArgs = false;
+                Role role = mutedRole.getAsRole();
+                if(role.getAsMention().equals("@everyone")) {
+                    settings.mutedRole = 0;
+                    event.reply("Muted users will not be assigned a role.").setEphemeral(true)
+                            .queue();
+                } else {
+                    settings.mutedRole = role.getIdLong();
+                    event.reply("Muted users will now be assigned to " + role.getAsMention() + ".")
+                            .setEphemeral(true).queue();
+                }
             }
             OptionMapping defaultPollColor = event.getOption("default-poll-color");
             if(defaultPollColor != null) {
@@ -134,14 +214,56 @@ public class DiscordListener extends ListenerAdapter {
                 event.reply("Auto-assign role set to " + role.getAsMention()).setEphemeral(true)
                         .queue();
             }
+            OptionMapping joinMessages = event.getOption("join-messages");
+            if(joinMessages != null) {
+                noArgs = false;
+                settings.joinMessages = joinMessages.getAsBoolean();
+            }
+            OptionMapping joinMessageChannel = event.getOption("join-message-channel");
+            if(joinMessageChannel != null) {
+                noArgs = false;
+                Channel channel = joinMessageChannel.getAsChannel();
+                if(channel instanceof VoiceChannel) {
+                    settings.joinMessages = false;
+                    event.reply("`join-message-channel` must be a text channel.").setEphemeral(true)
+                            .queue();
+                    return;
+                } else {
+                    settings.joinMessageChannel = channel.getIdLong();
+                }
+            } else if(joinMessages != null && joinMessages.getAsBoolean()) {
+                event.reply("`join-message-channel` is required when `join-messages` is true.")
+                        .setEphemeral(true).queue();
+                return;
+            }
+            OptionMapping leaveMessages = event.getOption("leave-messages");
+            if(leaveMessages != null) {
+                noArgs = false;
+                settings.leaveMessages = leaveMessages.getAsBoolean();
+            }
+            OptionMapping leaveMessageChannel = event.getOption("leave-message-channel");
+            if(leaveMessageChannel != null) {
+                noArgs = false;
+                Channel channel = leaveMessageChannel.getAsChannel();
+                if(channel instanceof VoiceChannel) {
+                    settings.leaveMessages = false;
+                    event.reply("`leave-message-channel` must be a text channel.")
+                            .setEphemeral(true).queue();
+                    return;
+                } else {
+                    settings.leaveMessageChannel = channel.getIdLong();
+                }
+            } else if(leaveMessages != null && leaveMessages.getAsBoolean()) {
+                event.reply("`leave-message-channel` is required when `leave-messages` is true.")
+                        .setEphemeral(true).queue();
+                return;
+            }
+
             if(noArgs) {
                 event.reply("No arguments were supplied.").setEphemeral(true).queue();
             } else {
-                try {
-                    Guilds.save(guild.getIdLong());
-                } catch(IOException e) {
-                    e.printStackTrace();
-                }
+                settings.save();
+                event.reply("Settings updated").setEphemeral(true).queue();
             }
         }
         default -> {}
@@ -187,13 +309,45 @@ public class DiscordListener extends ListenerAdapter {
 
     @Override
     public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
-        long roleId = Guilds.get(event.getGuild().getIdLong()).autoAssignRole;
-        Role role = event.getGuild().getRoleById(roleId);
-        if(role == null)
+        GuildSettings settings = Guilds.get(event.getGuild().getIdLong());
+        if(settings.joinMessages) {
+            TextChannel joinMessagesChannel =
+                    event.getGuild().getTextChannelById(settings.joinMessageChannel);
+            if(joinMessagesChannel != null) {
+                User user = event.getUser();
+                joinMessagesChannel.sendMessage(
+                        String.format("**%s#%s** has joined the server!", user.getName(),
+                                user.getDiscriminator())).queue();
+            }
+        }
+        long autoAssignRoleId = settings.autoAssignRole;
+        Role autoAssignRole = event.getGuild().getRoleById(autoAssignRoleId);
+        if(autoAssignRole == null)
             return;
-        event.getGuild()
-                .modifyMemberRoles(event.getMember(), List.of(role), Collections.emptyList())
-                .queue();
+        event.getGuild().modifyMemberRoles(event.getMember(), List.of(autoAssignRole),
+                Collections.emptyList()).queue();
+    }
+
+    @Override
+    public void onGuildMemberRemove(@NotNull GuildMemberRemoveEvent event) {
+        GuildSettings settings = Guilds.get(event.getGuild().getIdLong());
+        if(settings.leaveMessages) {
+            TextChannel leaveMessagesChannel =
+                    event.getGuild().getTextChannelById(settings.leaveMessageChannel);
+            if(leaveMessagesChannel != null) {
+                User user = event.getUser();
+                leaveMessagesChannel.sendMessage(
+                        String.format("**%s#%s** has left the server", user.getName(),
+                                user.getDiscriminator())).queue();
+            }
+        }
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if(Guilds.get(event.getGuild().getIdLong()).isMuted(event.getAuthor().getIdLong())) {
+            event.getMessage().delete().queue();
+        }
     }
 
     private static void handlePollCommand(SlashCommandInteractionEvent event) {
@@ -439,5 +593,42 @@ public class DiscordListener extends ListenerAdapter {
         if(!member.getRoles().contains(role))
             return;
         event.getGuild().modifyMemberRoles(member, Collections.emptyList(), List.of(role)).queue();
+    }
+
+    private static boolean checkCanMute(SlashCommandInteractionEvent event, Guild guild,
+            boolean unmute) {
+        GuildSettings settings = Guilds.get(guild.getIdLong());
+        if(settings.roleToMute == 0 && event.getUser().getIdLong() != guild.getOwnerIdLong()) {
+            event.reply("This command can only be used by the server owner.").setEphemeral(true)
+                    .queue();
+            return false;
+        } else if(event.getUser().getIdLong() != guild.getOwnerIdLong()) {
+            Member member = guild.getMemberById(event.getUser().getIdLong());
+            if(member == null) {
+                event.reply("You are not a member of this server.").setEphemeral(true).queue();
+                return false;
+            }
+            Role roleToMute = guild.getRoleById(settings.roleToMute);
+            if(roleToMute == null) {
+                event.reply("The role required to " + (unmute ? "unmute" : "mute") +
+                                " users is missing from the server.\n" + "Please contact the server owner.")
+                        .setEphemeral(true).queue();
+                return false;
+            }
+            boolean canInteract = false;
+            for(Role role : member.getRoles()) {
+                if(role.canInteract(roleToMute)) {
+                    canInteract = true;
+                    break;
+                }
+            }
+            if(!canInteract) {
+                event.reply(
+                                "You do not have permission to " + (unmute ? "unmute" : "mute") + " users.")
+                        .setEphemeral(true).queue();
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/dev/cvaugh/discordbot/GuildSettings.java
+++ src/main/java/dev/cvaugh/discordbot/GuildSettings.java
@@ -1,16 +1,103 @@
 package dev.cvaugh.discordbot;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+
 import java.awt.Color;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GuildSettings {
     public long id;
     public long roleToCreatePoll = 0;
+    public long roleToMute = 0;
+    public long mutedRole = 0;
     public int defaultEmbedColor = Integer.MAX_VALUE;
     public long autoAssignRole = 0;
+    public Map<Long, Long> muted = new HashMap<>();
+    public boolean joinMessages = false;
+    public long joinMessageChannel = 0;
+    public boolean leaveMessages = false;
+    public long leaveMessageChannel = 0;
 
     public Color getEmbedColor() {
         return defaultEmbedColor == Integer.MAX_VALUE ?
                 Config.getEmbedColor() :
                 new Color(defaultEmbedColor);
+    }
+
+    public boolean isMuted(long userId) {
+        if(muted.containsKey(userId) && muted.get(userId) < System.currentTimeMillis()) {
+            unmute(userId);
+        }
+        return muted.containsKey(userId);
+    }
+
+    public void mute(long userId, long until) {
+        muted.put(userId, until);
+        Guild guild = Main.jda.getGuildById(id);
+        if(guild == null)
+            return;
+        Member member = guild.getMemberById(userId);
+        if(member == null)
+            return;
+        Role role = guild.getRoleById(mutedRole);
+        if(role != null) {
+            guild.modifyMemberRoles(member, List.of(role), Collections.emptyList()).queue();
+        }
+        Main.scheduleMuteTask(id, userId, until);
+        try {
+            Main.writeGuildSettings(id);
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void unmute(long userId) {
+        if(muted.containsKey(userId)) {
+            muted.remove(userId);
+            Guild guild = Main.jda.getGuildById(id);
+            if(guild == null)
+                return;
+            Member member = guild.getMemberById(userId);
+            if(member == null)
+                return;
+            Role role = guild.getRoleById(mutedRole);
+            if(role != null) {
+                guild.modifyMemberRoles(member, Collections.emptyList(), List.of(role)).queue();
+            }
+            try {
+                Main.writeGuildSettings(id);
+            } catch(IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void rescheduleMuteTasks() {
+        List<Long> toRemove = new ArrayList<>();
+        for(long userId : muted.keySet()) {
+            if(muted.get(userId) < System.currentTimeMillis()) {
+                toRemove.add(userId);
+                continue;
+            }
+            Main.scheduleMuteTask(id, userId, muted.get(userId));
+        }
+        for(long userId : toRemove) {
+            muted.remove(userId);
+        }
+    }
+
+    public void save() {
+        try {
+            Main.writeGuildSettings(id);
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
